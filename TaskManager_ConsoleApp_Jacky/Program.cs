b57a365c5ec00ef2fc3892e8eb69c7f6636cb33b
JCruz6725 <john.cruz6725@gmail.com>
2025-09-16 14:49:58 -0500

    Add project files.

---
+++ TaskManager_ConsoleApp_Jacky/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager_ConsoleApp_Jacky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
