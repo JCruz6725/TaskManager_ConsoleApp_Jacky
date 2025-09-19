8db2ec3443f2ab24a29fc17fc8cc1e61b59fabcc
Jacqueline An Tran <jtran@nci.ent>
2025-09-19 10:25:37 -0500

    Added App class and moved main program code into it

---
+++ TaskManager_ConsoleApp_Jacky/App.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager_ConsoleApp_Jacky
+{
+    internal class App
+    {
+        TaskManager taskManager = new TaskManager();
+        TaskRenderer taskRenderer = new TaskRenderer();
+
+        public void Initialize()
+        {
+
+        }
+
+        public void MainLoop()
+        {
+            bool notExit = true;
+
+            while (notExit)
+            {
+                TaskManager manager = new TaskManager();
+                TaskRenderer display = new TaskRenderer();
+                display.DisplayMainMenu();
+
+                var createAction = Console.ReadLine();
+
+                switch (createAction)
+                {
+                    case "1":
+                        string taskTitle, taskDescription, taskStatus;
+                        DateTime taskDueDate;
+
+                        display.DisplayCreateItemMenu();
+
+                        display.DisplayTitleInput();
+                        taskTitle = Console.ReadLine();
+
+                        display.DisplayDescriptionInput();
+                        taskDescription = Console.ReadLine();
+
+                        display.DisplayDueDateInput();
+                        taskDueDate = DateTime.Parse(Console.ReadLine());
+
+                        display.DisplayStatusOption();
+                        taskStatus = Console.ReadLine();
+
+                        manager.CreateTask(taskTitle, taskDescription, taskDueDate, taskStatus);
+                        break;
+                    case "2":
+                        Console.WriteLine("You chose not to create");
+                        break;
+                    case "EXIT":
+                        notExit = false;
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid input, try again");
+                        break;
+
+                }
+            }
+        }
+
+        public void Shutdown()
+        {
+
+        }
+    }
+}
--- TaskManager_ConsoleApp_Jacky/Program.cs
+++ TaskManager_ConsoleApp_Jacky/Program.cs
@@ -4,54 +4,8 @@
     {
         static void Main(string[] args)
         {
-            bool notExit = true;
-
-            while (notExit)
-            {
-                TaskManager manager = new TaskManager();
-                TaskRenderer display = new TaskRenderer();
-                display.DisplayMainMenu();
-
-                var createAction = Console.ReadLine();
-
-                switch(createAction)
-                {
-                    case "1":
-                        string taskTitle, taskDescription, taskStatus;
-                        DateTime taskDueDate;
-
-                        display.DisplayCreateItemMenu();
-
-                        display.DisplayTitleInput();
-                        taskTitle = Console.ReadLine();
-
-                        display.DisplayDescriptionInput();
-                        taskDescription = Console.ReadLine();
-
-                        display.DisplayDueDateInput();
-                        taskDueDate = DateTime.Parse(Console.ReadLine());
-
-                        display.DisplayStatusOption();
-                        taskStatus = Console.ReadLine();
-
-                        manager.CreateTask(taskTitle, taskDescription, taskDueDate, taskStatus);
-                        break;
-                    case "2":
-                        Console.WriteLine("You chose not to create");
-                        break;
-                    case "EXIT":
-                        notExit = false;
-                        break;
-                    default:
-                        Console.WriteLine("Not a valid input, try again");
-                        break;
-
-                }
-            }
-
-           
-
-
+            App consoleApp = new App(); 
+            consoleApp.MainLoop();
         }
     }
 }
